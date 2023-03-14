8091e4ed800e371104b61c856090f5c27c6961ae
Campbell Rex <campbellrex@outlook.com>
2023-03-13 21:02:48 -0700

    Added shopping cart button to game catalog

---
+++ CPW219-eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly VideoGameContext _context;
+
+        public CartController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Game? gameToAdd = _context.Games.Where(g => g.GameID == id).SingleOrDefault();
+
+            if (gameToAdd == null)
+            {
+                // games with specified id does not exist
+                TempData["Message"] = "Sorry that game no longer exists on this website";
+                RedirectToAction("Index", "Games");
+            }
+            // ToDo: Add item to a shopping cart
+            TempData["Message"] = "Game added to cart successfully";
+            return RedirectToAction("Index", "Games");
+        }
+    }
+}
